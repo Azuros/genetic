f5dd8233d70b808af4f62754aafc76392ab2b437
Christian Jestel <christian.jestel@iml.fraunhofer.de>
2019-03-29 11:56:36 +0100

    Add mit-license to all files and remove some unused function.

--- src/genetic.cpp
+++ src/genetic.cpp
@@ -1,3 +1,24 @@
+/***
+ * Copyright 2019 Christian Jestel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #include "../include/genetic/genetic.hpp"
 
 #include <random>
@@ -75,15 +96,10 @@ void Genetic::visualize()
     std::list<Eigen::Vector2d> path;
     path.push_back(robot.getPosition());
 
-    //std::cout << "* * * Robot " << fitnessKumulative[0].first << " " << robot.getFitness() << " * * *" << std::endl;
-    //robot.getNetwork().printWeight();
-
     for (size_t i = 0; i < this->settings.getMaxSimulationSteps() && !robot.collidied(); ++i) {
         robot.updateLaser(this->world.getLines());
         robot.selectAction();
         path.push_back(robot.getPosition());
-
-        //std::cout << robot.getActionLinear() << "\t" << robot.getActionAngular() << std::endl;
 
         this->visualization.visualize(this->world, robot, path);
 
--- src/visualization.cpp
+++ src/visualization.cpp
@@ -1,3 +1,24 @@
+/***
+ * Copyright 2019 Christian Jestel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #include "../genetic/visualization.hpp"
 
 #include <iostream>
@@ -36,20 +57,11 @@ void Visualization::visualize(const World &world, const Robot &robot, const std:
         return;
     }
 
-
-
     fprintf(this->pipe, "set xrange [%f:%f]\n", world.getMinX() - 1.0, world.getMaxX() + 1.0);
     fprintf(this->pipe, "set yrange [%f:%f]\n", world.getMinY() - 1.0, world.getMaxY() + 1.0);
     fprintf(this->pipe, "plot '-' using 1:2:3 with circles lc 'magenta' fill solid border, '-' using 1:2:3:4 with vectors nohead lc 'blue', '-' using 1:2:3:4 with vectors nohead lc 'red', '-' using 1:2:3:4 with vectors nohead lc 'green', '-' using 1:2:3 with circles lc 'red', '-' using 1:2:3:4 with vectors lc 'red'\n");
 
     // create checkpoint
-    /* all for checking
-    for (size_t i = 0; i < world.getCheckpointSize(); ++i)
-    {
-        const Circle &circle = world.getCheckpointAt(i);
-        fprintf(this->pipe, "%f %f %f\n", circle.getX(), circle.getY(), circle.getRadius());
-    }
-    */
     fprintf(this->pipe, "%f %f %f\n", checkpoints[robot.getCurrentCheckpointIndex()].getX(), checkpoints[robot.getCurrentCheckpointIndex()].getY(), checkpoints[robot.getCurrentCheckpointIndex()].getRadius());
     fprintf(this->pipe, "EOF\n");
 
@@ -89,8 +101,6 @@ void Visualization::visualize(const World &world, const Robot &robot, const std:
     fprintf(this->pipe, "%f %f %f %f\n", robot.getX(), robot.getY(), direction.x(), direction.y());
 
     fprintf(this->pipe, "EOF\n");
-
-
 
     fflush(pipe);
 }
